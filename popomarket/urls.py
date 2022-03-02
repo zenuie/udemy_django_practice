a1b91b5145813661adb8e6aac6e8480c207c203a
zenuie <30676398+zenuie@users.noreply.github.com>
2022-03-02 16:05:44 +0800

    Update urls.py

--- popomarket/urls.py
+++ popomarket/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('carts/',include('carts.urls')),
     path('accounts/',include('accounts.urls')),
+
+    # orders
+    path('orders/',include('orders.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
